eff195a7bbf6b88d750c109ce86736925ea02234
Joseph Grech <joseph.grech@melitaltd.com>
2021-06-30 09:31:45 +0200

    Code cleaning.

--- web-client/src/app/service/sse.service.ts
+++ web-client/src/app/service/sse.service.ts
@@ -13,19 +13,22 @@ export class SseService {
   // A replacement for EventSource in order to support different Http Methods and a Request Body.
   // Requires npm install sse.js.
   public getObservable() : Observable<Page> {
-    console.log("Starting getSseObservable.");
     return new Observable((observer) => {
       let url = 'http://localhost:8080/flux';
-      let request = new PostRequest(3, 3);
-      console.log("request: " + JSON.stringify(request));
+      let requestBody = new PostRequest(3, 3);
+      let requestHeaders = {
+        'Content-Type': 'application/json',
+        'other-headdder-goes-here': 'with-other-value-here'
+      };
+      let httpMethod = 'POST';
 
-      let source = new SSE(url,
-        {headers: {
-            'Content-Type': 'application/json',
-            'other-header-goes-here': 'with-other-value-here'
-          },
-          payload: JSON.stringify(request),
-          method: 'POST'});
+      let request = {
+        headers: requestHeaders,
+        payload: JSON.stringify(requestBody),
+        method: httpMethod
+      };
+
+      let source = new SSE(url, request);
 
       source.onmessage = (event) => {
         observer.next(JSON.parse(event.data));
