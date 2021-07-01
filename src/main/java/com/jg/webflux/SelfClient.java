f9d5d0fd94d4f869e9fa16fb58461c8a357d39e7
Joseph Grech <josephgrech90@gmail.com>
2021-07-01 22:33:26 +0200

    Made SelfClient properties dynamic.

--- src/main/java/com/jg/webflux/SelfClient.java
+++ src/main/java/com/jg/webflux/SelfClient.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import reactivefeign.spring.config.ReactiveFeignClient;
 import reactor.core.publisher.Flux;
 
-@ReactiveFeignClient(name = "self-client", url = "http://localhost:8080")
+@ReactiveFeignClient(
+        name = "${reactive-feign.self-client.name:self-client}",
+        url = "${reactive-feign.self-client.url:http://localhost:8080}"
+)
 public interface SelfClient {
 
     @PostMapping("/flux")
